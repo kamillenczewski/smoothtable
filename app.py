15ea7cef370e0e478b629a84c9f366185ee637c7
Kamil Lenczewski <kamillenczewski2@gmail.com>
2025-01-23 19:23:02 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from table import createTable
+from color_condition import ColorCondition, Painter
+
+condition = ColorCondition(
+    type='row',
+    args='array',
+    method=lambda array: array[1].strip().endswith('a'),
+    color='red',
+    style='bold'
+)
+painter = Painter([condition])
+
+table = createTable(
+    columnLabels=['Id', 'First name', 'Last name', 'Favourite Color'],
+    rowLabels=['Row1', 'Row2', 'Row3', 'Row4', 'Row5'],
+    rows=[
+        ["1", 'Kamil', 'Lenczewski', 'Blue'],
+        ["2", 'Anastazja', 'Kasprzyk', 'Red'],
+        ["3", 'Karolina', 'Olawska', 'Black'],
+        ["4", 'Adam', 'Lewandowski', 'Black'],
+        ["5", 'Hania', 'Granat', 'Red']
+    ],
+    painter=painter
+)
+print(table)
